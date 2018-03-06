5a46f4cd8b8aef8bf3d5cc937b8121c883c19923
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-03-06 08:38:11 -0300

    termino da aula-01

--- src/ByteBank.Forum/Controllers/ContaController.cs
+++ src/ByteBank.Forum/Controllers/ContaController.cs
@@ -336,6 +336,27 @@ namespace ByteBank.Forum.Controllers
             return View("Login");
         }
 
+        public async Task<ActionResult> MinhaConta()
+        {
+            var modelo = new ContaMinhaContaViewModel();
+
+            var usuarioId = HttpContext.User.Identity.GetUserId();
+            var usuario = await UserManager.FindByIdAsync(usuarioId);
+
+            modelo.NomeCompleto = usuario.NomeCompleto;
+            modelo.NumeroDeCelular = usuario.PhoneNumber;
+            modelo.HabilitarAutenticacaoDeDoisFatores = usuario.TwoFactorEnabled;
+            modelo.NumeroDeCelularConfirmado = usuario.PhoneNumberConfirmed;
+
+            return View(modelo);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> MinhaConta(ContaMinhaContaViewModel modelo)
+        {
+            return View();
+        }
+
         private void AdicionaErros(IdentityResult resultado)
         {
             foreach (var erro in resultado.Errors)
---
+++ src/ByteBank.Forum/ViewModels/ContaMinhaContaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ByteBank.Forum.ViewModels
+{
+    public class ContaMinhaContaViewModel
+    {
+        [Required]
+        [Display(Name ="Nome completo")]
+        public string NomeCompleto { get; set; }
+
+        [Display(Name = "Número de celular")]
+        public string NumeroDeCelular { get; set; }
+
+        [Display(Name = "Habilitar autenticação de dois fatores")]
+        public bool HabilitarAutenticacaoDeDoisFatores { get; set; }
+
+        public bool NumeroDeCelularConfirmado { get; set; }
+    }
+}
